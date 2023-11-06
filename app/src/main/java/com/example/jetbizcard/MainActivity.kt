ef66c0089fd43e29343a1e84d8fd44dc04d84f55
Manuel-Francisco-Mendes <manuelfranciscomendes25@gmail.com>
2023-11-06 19:07:05 +0200

    Create profile picture and devider

--- app/src/main/java/com/example/jetbizcard/MainActivity.kt
+++ app/src/main/java/com/example/jetbizcard/MainActivity.kt
@@ -3,15 +3,20 @@ package com.example.jetbizcard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jetbizcard.ui.theme.JetBizCardTheme
@@ -39,15 +44,49 @@ fun CreateBizCard(){
     Surface(modifier = Modifier
         .fillMaxWidth()
         .fillMaxHeight()) {
-        Card(modifier = Modifier
-            .width(200.dp)
-            .height(390.dp)
-            .padding(12.dp),
-            shape = RoundedCornerShape(corner = CornerSize(15.dp)),
-            elevation = 4.dp
-        ) {
 
-        }
+            Card(modifier = Modifier
+                .width(200.dp)
+                .height(390.dp)
+                .padding(12.dp),
+                shape = RoundedCornerShape(corner = CornerSize(15.dp)),
+                elevation = 4.dp
+            ) {
+
+                Column(modifier = Modifier.fillMaxWidth(),
+                    verticalArrangement = Arrangement.Top,
+                    horizontalAlignment = Alignment.CenterHorizontally) {
+
+                    CreateProfilePicture()
+
+                    Divider(modifier = Modifier
+                        .padding(0.dp,5.dp,0.dp,5.dp),
+                        thickness = 2.dp)
+
+                }
+            }
+
+
+
+    }
+
+}
+
+@Composable
+fun CreateProfilePicture(){
+
+    Surface(modifier = Modifier
+        .size(150.dp)
+        .padding(5.dp),
+        shape = CircleShape,
+        border = BorderStroke(2.dp, Color.LightGray),
+        elevation = 4.dp,
+        color = MaterialTheme.colors.onSurface.copy(alpha = 0.5f)
+    ) {
+        Image(painter = painterResource(id = R.drawable.profile_image),
+            contentDescription = "Profile picture",
+            modifier = Modifier.size(135.dp),
+            contentScale = ContentScale.Crop)
     }
 
 }
